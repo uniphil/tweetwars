4bf1e0f3815d1774f96482c1e02e060a32e1b6fc
Philip Schleihauf <uniphil@gmail.com>
2013-04-15 10:38:27 -0400

    blah

--- parser.js
+++ parser.js
@@ -45,8 +45,8 @@ start.add_transition(new Transition(/\./, num_dot));
 num_dot.add_transition(new Transition(/\d/, floating));
 floating.add_transition(new Transition(/\d/, floating));
 // names
-start.add_transition(new Transition(/[co]/, short_name));
-short_name.add_transition(new Transition(/[\-\|]/, long_name));
+start.add_transition(new Transition(/[ro]/, short_name));
+short_name.add_transition(new Transition(/['\|]/, long_name));
 // parens
 start.add_transition(new Transition(/\(/, o_paren));
 start.add_transition(new Transition(/\)/, c_paren));
@@ -85,6 +85,8 @@ var feed_the_machine = function(expression) {
 };
 
 
+// BUG! .1 tokenizes to 1
+// BUG! whitespace at the end
 
 //////////////////// PARSER //////////////////////
 
@@ -104,124 +106,44 @@ var op_fns = d3.map({
     ">": function(l, r) { return function(cx) { return l(cx) > r(cx); }; },
 });
 
-var compile_strategy = function(strategy) {
+var compile_expression = function(strategy) {
     // in-line replace tokens with a tree of callables
     tokens = feed_the_machine(strategy);
     // start with literals and names
     for (var t = 0; t < tokens.length; t++) {
         if (tokens[t][0] === "literal") {
             value = parseFloat(tokens[t][1]);
-            tokens[t] = function(context) { return value; };
+            tokens[t] = (function(value) {
+                return function(context) { return value; };
+            })(value);
         } else if (tokens[t][0] === "name") {
             name = tokens[t][1]
-            tokens[t] = function(context) { return context.get(name); };
+            tokens[t] = (function(name) {
+                return function(context) { return context.get(name); };
+            })(name);
         }
     }
     // parens
     // operators
     var exiter = 0;
     for (var o = 0; o < op_order.length; o++) {
-        console.log('checking for o ' + op_order[o])
         for (var t = 0; t < tokens.length;) {
-            console.log('against token ' + tokens[t][1]);
             if (exiter++ > 100) throw "exit!";
-            if ((tokens[t] instanceof Array) && tokens[t][0] === "operator") {
-                console.log('found operator!');
+            if (tokens[t][0] === "operator") {
                 if (op_order[o].test(tokens[t][1])) {
-                    console.log(tokens);
-                    console.log('WOOOO matched operator!');
-                    group_index = t-1; // assuming three tokens for binary op
-                    group = tokens.splice(t-1, t+1);
-                    console.log('token, again, ' + group[1][1]);
+                    group = tokens.splice(t-1, t+2);
                     callable = op_fns.get(group[1][1])(group[0], group[2]); // l, r
                     tokens.splice(t-1, 0, callable);
-                    console.log(tokens);
                 } else {
-                    console.log('operator no match for me');
                     t++;
                 }
             } else {
-                console.log('not an operator, moving on...');
                 t++;
             }
         }
     }
-    return tokens;
+    return tokens[0];
 }
-
-
-c_in_strategy = "2 + .1 * 3.2"; // propel toward center
-var strat = compile_strategy(c_in_strategy)
-printline(strat.join('\n'));
-console.log(strat);
-// printline(compile_strategy(c_in_strategy()));
-
-// FAILING CASES: '.' is evaluated as a literal
-
-
-// var unary_funcs = d3.map({
-//     "-": function(n) { return -n; },  // negate
-// });
-
-// var binary_funcs = d3.map({
-//     "-": function(l, r) { return l - r; },
-//     "+": function(l, r) { return l + r; },
-//     "*": function(l, r) { return l * r; },
-//     "/": function(l, r) { return l / r; },
-//     "^": function(l, r) { return Math.pow(l, r); },
-//     "<": function(l, r) { return int(l < r); },
-//     ">": function(l, r) { return int(l > r); },
-// });
-
-// var printline = function(t) { document.write(t); document.write('\n'); };
-
-
-// printline('literal -> 1: ' + Literal('1')());
-// printline('literal -> 2: ' + (Literal('1')() + Literal('1')()));
-
-// printline('name    -> 1: ' + Name('o')(d3.map({'o': 1})));
-
-// var Unary = function(rep) {
-//     return function(thing) { return unary_funcs.get(rep)(thing); };
-// };
-// printline('unary  -> -1: ' + Unary('-')(Literal('1')()));
-
-// var Binary = function(rep) {
-//     return function(things) {
-//         return binary_funcs.get(rep)(things[0], things[1])
-//     };
-// };
-// printline('binary  -> 2: ' + Binary('+')([Literal('1')(), Literal('1')()]));
-
-// var Func = function(rep, conf) {
-//     // functions are unary
-//     return function(thing) {
-//         console.log('execing');
-//         console.log('fn: ' + config_funcs.get(rep));
-//         console.log('')
-//         return config_funcs.get(rep)(conf, thing);
-//     };
-// };
-// printline('func    -> 1: ' + Func('/', Literal('2'))(Literal('0.5')()));
-// printline('func   -> .5: ' + Func('j', Literal('0.5'))(Literal('.5')()));
-
-// var Feeder = function(express) {
-//     // get a char at a time
-//     this.express = express;
-//     this.pos = 0;
-//     this.next_char = function() {
-//         if (this.pos === this.express.length) {
-//             return null;
-//         }
-//         var ch = this.express.slice(this.pos, this.pos + 1);
-//         this.pos++;
-//         return ch;
-//     }
-// };
-
-// printline(Binary('/')([Func('j', Unary('-')(Literal('.1')()))(Literal('.2')()), Literal('5')()]));
-
-
 
 
 
